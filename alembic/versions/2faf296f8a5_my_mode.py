40b64214123774a941b32ac9f49e540e31b72811
Mahesh Chandra <mmahesh@iitj.ac.in>
2013-11-11 10:16:24 +0530

    added alembic

---
+++ alembic/versions/2faf296f8a5_my_mode.py
@@ -0,0 +1,22 @@
+"""my mode
+
+Revision ID: 2faf296f8a5
+Revises: 1ab5419e503
+Create Date: 2013-11-10 23:10:43.179712
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2faf296f8a5'
+down_revision = '1ab5419e503'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- some/models.py
+++ some/models.py
@@ -21,7 +21,5 @@ Base = declarative_base()
 class MyModel(Base):
     __tablename__ = 'models'
     id = Column(Integer, primary_key=True)
-    name = Column(Text)
+    name1 = Column(Text)
     value = Column(Integer)
-
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
